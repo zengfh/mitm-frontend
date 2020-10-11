f41dcd94f2974268c0f05c928ce1ff59e8718d92
zengfh <hankzeng.95@gmail.com>
2020-10-11 11:30:07 -0500

    Updates

---
+++ precache-manifest.e122fcf9accd173c27f2e5db474b950f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a000df0dd87bf6a43aae8e2d626c0b",
+    "url": "/mitm-frontend/index.html"
+  },
+  {
+    "revision": "6cb1d0fe730d38637817",
+    "url": "/mitm-frontend/static/css/main.52dd4725.chunk.css"
+  },
+  {
+    "revision": "6d110da24a2910ac86a4",
+    "url": "/mitm-frontend/static/js/2.eb9798d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mitm-frontend/static/js/2.eb9798d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cb1d0fe730d38637817",
+    "url": "/mitm-frontend/static/js/main.a98cc6d5.chunk.js"
+  },
+  {
+    "revision": "644e1ad5950e038d2a94",
+    "url": "/mitm-frontend/static/js/runtime-main.2cfc1289.js"
+  }
+]);
