1358dc0843e43bbee9510217fa469d85460a7de8
zengfh <hankzeng.95@gmail.com>
2020-10-18 16:36:26 -0500

    Updates

--- precache-manifest.89f3a6fead120c40d0a9e848fc1137d5.js
+++ precache-manifest.89f3a6fead120c40d0a9e848fc1137d5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1e5116fa53d15565823f96b9c6a87304",
+    "revision": "c1efcd1a4f30cfc50033c7e64ecb2635",
     "url": "/mitm-frontend/index.html"
   },
   {
-    "revision": "81a5d19198051e035e5c",
+    "revision": "8a6397a88c6151784175",
     "url": "/mitm-frontend/static/css/main.52dd4725.chunk.css"
   },
   {
-    "revision": "6b42c3e3839b87bb4da2",
-    "url": "/mitm-frontend/static/js/2.f60d1797.chunk.js"
+    "revision": "6d110da24a2910ac86a4",
+    "url": "/mitm-frontend/static/js/2.eb9798d6.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/mitm-frontend/static/js/2.f60d1797.chunk.js.LICENSE.txt"
+    "url": "/mitm-frontend/static/js/2.eb9798d6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "81a5d19198051e035e5c",
-    "url": "/mitm-frontend/static/js/main.52472875.chunk.js"
+    "revision": "8a6397a88c6151784175",
+    "url": "/mitm-frontend/static/js/main.f24fd9d2.chunk.js"
   },
   {
     "revision": "644e1ad5950e038d2a94",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mitm-frontend/precache-manifest.06bbd7c40614ec068672d081ec4b21d5.js"
+  "/mitm-frontend/precache-manifest.89f3a6fead120c40d0a9e848fc1137d5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mitm-frontend/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
